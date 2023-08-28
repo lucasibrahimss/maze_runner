87bf4af2923b5bae2cd2db94c6a101c8b24fd855
lucasibrahimss <142946220+lucasibrahimss@users.noreply.github.com>
2023-08-28 17:03:12 +0000

    funcao de encontrar o caminho funcionando

--- maze_runner.cpp
+++ maze_runner.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <iostream>
 #include <sstream>
+#include <queue>
+#include <utility>
 
 // Matriz de char representnado o labirinto
 char** maze; // Voce também pode representar o labirinto como um vetor de vetores de char (vector<vector<char>>)
@@ -20,24 +22,6 @@ struct pos_t {
 	std::vector<std::vector<char>> labirinto;
 };
 
-// Estrutura de dados contendo as próximas
-// posicões a serem exploradas no labirinto
-std::stack<pos_t> valid_positions;
-/* Inserir elemento: 
-
-	 pos_t pos;
-	 pos.i = 1;
-	 pos.j = 3;
-	 valid_positions.push(pos)
- */
-// Retornar o numero de elementos: 
-//    valid_positions.size();
-// 
-// Retornar o elemento no topo: 
-//  valid_positions.top(); 
-// 
-// Remover o primeiro elemento do vetor: 
-//    valid_positions.pop();
 
 
 // Função que le o labirinto de um arquivo texto, carrega em 
@@ -106,49 +90,65 @@ void print_maze() {
 		}
 		printf("\n");
 	}
+};
+
+std::vector<std::pair<int, int>> encontrarSaidaLabirinto(std::vector<std::vector<char>>& labirinto, int linhaInicial, int colunaInicial) {
+    int linhas = labirinto.size();
+    int colunas = labirinto[0].size();
+	std::vector<std::pair<int, int>> valid_pos;
+
+    // Criar uma fila para BFS
+    std::queue<std::pair<int, int>> fila;
+    fila.push(std::pair<int, int>(linhaInicial, colunaInicial));
+
+    // Movimentos possíveis: cima, baixo, esquerda, direita
+    int movimentosLin[] = {-1, 1, 0, 0};
+    int movimentosCol[] = {0, 0, -1, 1};
+
+    while (!fila.empty()) {
+        int linha = fila.front().first;
+        int coluna = fila.front().second;
+        fila.pop();
+
+        // Checar se a posição atual é a saída
+        if (labirinto[linha][coluna] == 's') {
+        	return valid_pos;
+        }
+
+        // Marcar a posição como visitada
+        labirinto[linha][coluna] = '#';
+
+        // Tentar mover para todas as direções possíveis
+        for (int i = 0; i < 4; ++i) {
+            int novaLinha = linha + movimentosLin[i];
+            int novaColuna = coluna + movimentosCol[i];
+
+            // Checar se a nova posição está dentro do labirinto e não é um obstáculo
+            if (novaLinha >= 0 && novaLinha < linhas && novaColuna >= 0 && novaColuna < colunas && labirinto[novaLinha][novaColuna] != '#') {
+                fila.push(std::pair<int, int>(novaLinha, novaColuna));
+				valid_pos.push_back(std::pair<int, int>(novaLinha, novaColuna));
+				
+            }
+        }
+    }
+
+
+
 }
 
 
-// Função responsável pela navegação.
-// Recebe como entrada a posição initial e retorna um booleando indicando se a saída foi encontrada
-bool walk(pos_t pos) {
-	
-	// Repita até que a saída seja encontrada ou não existam mais posições não exploradas
-		// Marcar a posição atual com o símbolo '.'
-		// Limpa a tela
-		// Imprime o labirinto
-		
-		/* Dado a posição atual, verifica quais sao as próximas posições válidas
-			Checar se as posições abaixo são validas (i>0, i<num_rows, j>0, j <num_cols)
-		 	e se são posições ainda não visitadas (ou seja, caracter 'x') e inserir
-		 	cada uma delas no vetor valid_positions
-		 		- pos.i, pos.j+1
-		 		- pos.i, pos.j-1
-		 		- pos.i+1, pos.j
-		 		- pos.i-1, pos.j
-		 	Caso alguma das posiçÕes validas seja igual a 's', retornar verdadeiro
-	 	*/
-
-		
-	
-		// Verifica se a pilha de posições nao esta vazia 
-		//Caso não esteja, pegar o primeiro valor de  valid_positions, remove-lo e chamar a funçao walk com esse valor
-		// Caso contrario, retornar falso
-		if (!valid_positions.empty()) {
-			pos_t next_position = valid_positions.top();
-			valid_positions.pop();
-		}
-	return false;
-}
 
 int main(int argc, char* argv[]) {
 	// carregar o labirinto com o nome do arquivo recebido como argumento
 	pos_t initial_pos = load_maze(argv[1]);
-	std::cout<<initial_pos.labirinto[8][0] << std::endl;
 	// chamar a função de navegação
-	bool exit_found = walk(initial_pos);
+	std::vector<std::pair<int, int>> exit_found = encontrarSaidaLabirinto(initial_pos.labirinto, initial_pos.i, initial_pos.j);
+	for(int i=0; i<exit_found.size(); i++)
+	{
+		std::cout << exit_found[i].first << "    -    " << exit_found[i].second << std::endl;
+	}
 	
 	// Tratar o retorno (imprimir mensagem)
 	
 	return 0;
-}
+};
